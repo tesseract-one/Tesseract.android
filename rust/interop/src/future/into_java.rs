8c92feda519dfa28e18cb05209395fa548a8cc45
Daniel Leping <dileping@gmail.com>
2023-02-28 00:24:17 +0000

    Added capability for the futures to be converted to java from Box and Pin_Box

--- rust/interop/src/future/into_java.rs
+++ rust/interop/src/future/into_java.rs
@@ -1,22 +1,34 @@
-use std::fmt::Display;
-use std::future::Future;
-use std::task::{Wake, Context, Poll};
-use std::pin::Pin;
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::{
+    pin::Pin,
+    future::Future,
+    result::Result,
+    fmt::Display,
+    sync::{Arc, Mutex, MutexGuard},
+    task::{Wake, Context, Poll},
+};
 
-use jni::JNIEnv;
-use jni::objects::GlobalRef;
+use jni::{
+    JNIEnv,
+    objects::GlobalRef
+};
 
 use crate::contexted_global::ContextedGlobal;
 
 use super::completable_future::JCompletableFuture;
 
-struct Waker<E: Send + Display + 'static, F: Future<Output = std::result::Result<GlobalRef, E>> + Send + 'static> {
-    rs: Mutex<Pin<Box<F>>>,
-    j: ContextedGlobal
+struct Waker<E, F> where
+    E: Send + Display + 'static,
+    F: ?Sized + Send + 'static,
+    F: Future<Output = Result<GlobalRef, E>> {
+        rs: Mutex<Pin<Box<F>>>,
+        j: ContextedGlobal
 }
 
-impl<E: Send + Display, F: Future<Output = std::result::Result<GlobalRef, E>> + Send> Waker<E, F>  {
+impl<E, F> Waker<E, F> where
+    E: Send + Display,
+    F: ?Sized + Send,
+    F: Future<Output = Result<GlobalRef, E>> {
+
     pub fn new(rs: Pin<Box<F>>, j: ContextedGlobal) -> Self {
         Self { rs: Mutex::new(rs), j: j }
     }
@@ -26,7 +38,11 @@ impl<E: Send + Display, F: Future<Output = std::result::Result<GlobalRef, E>> + 
     }
 }
 
-impl<E: Send + Display, F: Future<Output = std::result::Result<GlobalRef, E>> + Send> Wake for Waker<E, F> {
+impl<E, F> Wake for Waker<E, F> where
+    E: Send + Display,
+    F: ?Sized + Send,
+    F: Future<Output = Result<GlobalRef, E>> {
+
     fn wake(self: Arc<Self>) {
         self.j.do_in_context_rret(64, |env, object| {
             let jfut = JCompletableFuture::from_env(&env, object);
@@ -51,16 +67,21 @@ impl<E: Send + Display, F: Future<Output = std::result::Result<GlobalRef, E>> + 
 }
 
 pub trait FutureJava {
-    fn into_java<'a: 'b, 'b>(self, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b>;
+    fn into_java<'a: 'b, 'b>(self, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b> where Self: Sized;
+    fn boxed_into_java<'a: 'b, 'b>(self: Box<Self>, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b>;
+    fn pinned_into_java<'a: 'b, 'b>(self: Pin<Box<Self>>, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b>;
 }
 
-impl<E: Send + Display + 'static, F: Send + 'static> FutureJava for F where F: Future<Output = std::result::Result<GlobalRef, E>> {
-    fn into_java<'a: 'b, 'b>(self, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b> {
+impl<E, F> FutureJava for F where
+    E: Send + Display + 'static,
+    F: ?Sized + Send + 'static,
+    F: Future<Output = Result<GlobalRef, E>> {
+
+    fn pinned_into_java<'a: 'b, 'b>(self: Pin<Box<Self>>, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b> {
         let ljfut = JCompletableFuture::new(env).unwrap();
         let gjfut = ContextedGlobal::from_local(env, *ljfut).unwrap();
 
-        let boxed = Box::pin(self);
-        let waker = Arc::new(Waker::new(boxed, gjfut));
+        let waker = Arc::new(Waker::new(self, gjfut));
         let mut fguard = waker.guard();
         
         let waker = Arc::clone(&waker).into();
@@ -78,5 +99,13 @@ impl<E: Send + Display + 'static, F: Send + 'static> FutureJava for F where F: F
         }
 
         ljfut
+    }
+
+    fn into_java<'a: 'b, 'b>(self, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b> where Self: Sized {
+        Box::pin(self).pinned_into_java(env)
+    }
+
+    fn boxed_into_java<'a: 'b, 'b>(self: Box<Self>, env: &'b JNIEnv<'a>) -> JCompletableFuture<'a, 'b> {
+        unsafe { Pin::new_unchecked(self) }.pinned_into_java(env)
     }
 }
